1b8884065af6ad2cac2bb2485db9a4bea64bdb9a
marirq <mrq.vet@gmail.com>
2020-01-21 09:58:27 -0300

    heading to more new implements

--- graphs.R
+++ graphs.R
@@ -42,37 +42,18 @@ assR <- testR %>%
 
 library(GGally)
 a <- ggpairs(assR[,c('school', 'dogs')], ggplot2::aes(colour= school), legend = c(1, 2))
-
-# multiple plots - http://bit.ly/2tzsoli ###
-# Plot separate ggplot figures in a loop.
-library(ggplot2)
-
-# Make list of variable names to loop over.
-var_list = combn(names(iris)[1:3], 2, simplify=FALSE)
-
-# Make plots.
-plot_list = list()
-for (i in 1:3) {
-  p = ggplot(iris, aes_string(x=var_list[[i]][1], y=var_list[[i]][2])) +
-    geom_point(size=3, aes(colour=Species))
-  plot_list[[i]] = p
+plot(assR$`outer walls`, assR$dogs)
+a <- assR[,1:5] %>%
+  mutate(dogs = assR$dogs)
+df.list <- list()
+for (i in 1:41) {
+  df.list[[i]] <- melt(assR, id.vars = names(assR[,i]))
 }
 
-# Save plots to tiff. Makes a separate file for each plot.
-for (i in 1:3) {
-  file_name = paste("iris_plot_", i, ".tiff", sep="")
-  tiff(file_name)
-  print(plot_list[[i]])
-  dev.off()
-}
-
-# Another option: create pdf where each page is a separate plot.
-pdf("plots.pdf")
-for (i in 1:3) {
-  print(plot_list[[i]])
-}
-dev.off()
-
+# multiple plots? - http://bit.ly/2tzsoli ####
+var_list = combn(names(a), 1, simplify=FALSE)
+ggplot(df.list[[1]], aes_(fill=df.list[[1]][1], y=df.list[[1]][3], x=df.list[[1]][2])) + 
+  geom_bar()#position="dodge", stat="identity")
 
 a <- melt(assR, id.vars = 'raca')
 summary(assR[,-42])
